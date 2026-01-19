c5478053dfe8ee0ac13a0e39d2b2c8ec1d2107a2
sbeastian <sebastianmondaca95@gmail.com>
2026-01-19 17:07:15 -0300

    otro intento

---
+++ Backend/microservice-user/src/main/java/com/microservice_user/config/GlobalCorsConfig.java
@@ -0,0 +1,26 @@
+package com.microservice_user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "https://dashboard-rrhh-front-1056553941867.southamerica-west1.run.app",
+                        "https://dashboard-rrhh-front-1056553941867.web.app"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
--- Backend/microservice-user/src/main/java/com/microservice_user/controller/UserController.java
+++ Backend/microservice-user/src/main/java/com/microservice_user/controller/UserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "https://dashboard-rrhh-front-1056553941867.southamerica-west1.run.app", "https://dashboard-rrhh-front-7jrwjdnsoq-tl.a.run.app"}, allowCredentials = "true")
 public class UserController {
 
     @Autowired
